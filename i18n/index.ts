0a6f618a7db359e64641416231e3831fe037f24f
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-26 18:48:40 -0300

    i18n config

---
+++ i18n/index.ts
@@ -0,0 +1,46 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import * as Localization from "expo-localization";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import translationEn from "./locales/en-US/translation.json";
+import translationPt from "./locales/pt-BR/translation.json";
+// import translationRu from "./locales/ru-RU/translation.json";
+// import translationZh from "./locales/zh-CN/translation.json";
+// import translationEs from "./locales/es-ES/translation.json";
+
+// Configuração das traduções
+const resources = {
+  "pt-BR": { translation: translationPt },
+  "en-US": { translation: translationEn },
+  //   "ru-RU": { translation: translationRu },
+  //   "zh-CN": { translation: translationZh },
+  //   "fr-FR": { translation: translationFr },
+  //   "it-IT": { translation: translationIt },
+  //   "hi-IN": { translation: translationHi },
+};
+
+const initI18n = async () => {
+  let savedLanguage = await AsyncStorage.getItem("language");
+
+  if (!savedLanguage) {
+    savedLanguage = Localization.locale;
+  }
+
+  i18n.use(initReactI18next).init({
+    compatibilityJSON: "v3",
+    resources,
+    lng: savedLanguage,
+    fallbackLng: "pt-BR",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+  // Configura a direção do texto (LTR ou RTL)
+  // I18nManager.allowRTL(false);
+  // I18nManager.forceRTL(Localization.isRTL);
+};
+
+initI18n();
+
+export default i18n;
