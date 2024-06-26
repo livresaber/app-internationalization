7a8dad59fa58f165c2328b3ed492df289acd1ba7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2024-06-26 18:47:50 -0300

    import i18n in layout app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import '@/i18n';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
